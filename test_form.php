f6efcf0765a2d33d8da392a7fe0fa55568a62e16
System Administrator <root@Seans-MacBook-Pro.local>
2016-05-17 14:54:43 -0400

    my only question so far is how to auto-restart apache on save

---
+++ test_form.php
@@ -0,0 +1,24 @@
+
+
+<?php
+
+$firstName = $_POST['fname'];
+$lastName = $_POST['lname'];
+
+
+
+
+// echo $firstName;
+// echo $lastName;
+
+
+echo $firstName . "'s last name is " . $lastName . '<br>';
+
+
+$tenceString = <<<IDK
+My friend's name is $firstName, like many other people they also have a last name which is $lastName<br>
+IDK;
+
+echo $tenceString
+
+?>
